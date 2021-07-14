92db0fcac9e8786a3565d80321777b7e66819210
Yashraj Dawkhar <yashrajdawkhar@gmail.com>
2021-07-15 03:14:18 +0530

    header file

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ const studentsRouter = require("./routers/students");
 const app = express();
 app.use(studentsRouter);
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET , PUT , POST , DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
+    next(); // Important
+})
 
 app.listen(port, () => {
     console.log(`connection is setup at ${port}`);
