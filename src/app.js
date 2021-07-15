00f67a723db364a455a9db50580421c967f95854
Yashraj Dawkhar <yashrajdawkhar@gmail.com>
2021-07-15 14:17:49 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -2,16 +2,12 @@ const express = require("express");
 const db = require("./db/Connection");
 const port = process.env.PORT || 4000;
 const studentsRouter = require("./routers/students");
+var cors = require('cors')
 
 const app = express();
 app.use(studentsRouter);
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers","Origin,Content-Type,X-Requested-With,Accept,Authorization");
-    next(); // Important
-})
+app.use(cors())
 
 app.listen(port, () => {
     console.log(`connection is setup at ${port}`);
