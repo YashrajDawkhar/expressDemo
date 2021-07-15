552d34a8b5c80a0848acdbbef7c0c15dd48f5480
Yashraj Dawkhar <yashrajdawkhar@gmail.com>
2021-07-15 17:07:01 +0530

    u

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const cors = require('cors');
 const db = require("./db/Connection");
 const port = process.env.PORT || 4000;
 const studentsRouter = require("./routers/students");
-var cors = require('cors')
+
+app.use(cors({
+    origin: '*'
+}));
 
 const app = express();
 app.use(studentsRouter);
 
-app.use(cors())
+
 
 app.listen(port, () => {
     console.log(`connection is setup at ${port}`);
