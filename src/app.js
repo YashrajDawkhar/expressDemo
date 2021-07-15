b9ae9bfe910f927297180519fb6a90c5d7031976
Yashraj Dawkhar <yashrajdawkhar@gmail.com>
2021-07-15 17:23:34 +0530

    u

--- src/app.js
+++ src/app.js
@@ -1,12 +1,9 @@
 const express = require("express");
-const cors = require('cors');
+
 const db = require("./db/Connection");
 const port = process.env.PORT || 4000;
 const studentsRouter = require("./routers/students");
 
-app.use(cors({
-    origin: '*'
-}));
 
 const app = express();
 app.use(studentsRouter);
